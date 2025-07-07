4ce52e74206f3fb10f002cd0f95a0ada2b29ffa0
Lenovo_Youssouf <ytangara2003@gmail.com>
2025-07-07 18:36:53 +0100

    background-degradé appliqué a toute les page necessaire + backgound changé

--- src/app/components/gallery-section/gallery-section.component.ts
+++ src/app/components/gallery-section/gallery-section.component.ts
@@ -11,30 +11,14 @@ import { TranslateService, TranslateModule } from '@ngx-translate/core';
 export class GallerySectionComponent implements OnInit, OnDestroy {
   images = [
     'assets/images/Tennis.jpg',
-    'assets/images/g1.jpg',
-    'assets/images/g2.jpg',
-    'assets/images/g3.jpg',
-    'assets/images/g4.jpg',
-    'assets/images/g5.jpg',
-    'assets/images/g6.jpg',
-    'assets/images/g7.jpg',
-    'assets/images/g8.jpg',
-    'assets/images/g9.jpg',
-    'assets/images/g10.jpg',
-    'assets/images/g11.jpg',
-    'assets/images/g12.jpg',
-    'assets/images/g13.jpg',
-    'assets/images/g14.jpg',
-    'assets/images/g15.jpg',
-    'assets/images/g16.jpg',
     'assets/images/Piscine.jpg',
     'assets/images/Piscine1.jpg',
     'assets/images/Padel.jpg',
-    'assets/images/volley.jpg',
     'assets/images/Foot.jpg',
     'assets/images/Basket.jpg',
     'assets/images/Athletisme.jpg',
     'assets/images/Arts-Martiaux.jpg',
+    'assets/images/volley.jpg',
     'assets/images/Petanque.jpg'
   ];
   private langSub: any;
